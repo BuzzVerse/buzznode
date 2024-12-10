c8ea190d1b5b151761ecdedb77b18ec5ac1faf17
Olaf Bykowski <olafbykowski1@gmail.com>
2024-12-10 12:42:05 +0100

    feat(LoraWAN): Initial radio config and lorawan support
    Added lora alias and config to .overlay, and added network joining and sending sample data. Restart if network joining failed

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,112 @@
-#include "etl/circular_buffer.h"
-
 #include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/drivers/sensor_data_types.h>
-#include <zephyr/dsp/print_format.h>
+#include <zephyr/lorawan/lorawan.h>
 #include <zephyr/kernel.h>
-#include <zephyr/rtio/rtio.h>
+#include <zephyr/sys/reboot.h>
 
-const struct device *const dev = DEVICE_DT_GET_ANY(bosch_bme280);
-SENSOR_DT_READ_IODEV(iodev, DT_COMPAT_GET_ANY_STATUS_OKAY(bosch_bme280),
-                     {SENSOR_CHAN_AMBIENT_TEMP, 0}, {SENSOR_CHAN_HUMIDITY, 0},
-                     {SENSOR_CHAN_PRESS, 0});
+#define LORAWAN_DEV_EUI			{ 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0xC4, 0x52 }
+#define LORAWAN_JOIN_EUI		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+#define LORAWAN_APP_KEY			{ 0xAA, 0xCF, 0x7B, 0xEA, 0x16, 0xC2, 0xF1, 0xDD, 0x75, 0x0F, 0xAA, 0xE9, 0x86, 0x94, 0xB9, 0x36 }
 
-RTIO_DEFINE(ctx, 1, 1);
-int main() {
-  etl::circular_buffer<int32_t, 100> temp_buffer;
+#define DELAY K_MSEC(10000)
 
-  printk("Hello %s\n", CONFIG_BOARD);
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
 
-  if (dev == NULL) {
-    return 0;
-  }
+LOG_MODULE_REGISTER(lorawan_class_a);
 
-  while (1) {
-    uint8_t buf[128];
+uint8_t data[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
 
-    int rc = sensor_read(&iodev, &ctx, buf, 128);
+static void dl_callback(uint8_t port, bool pending, int16_t rssi, int8_t snr, uint8_t len, const uint8_t *hex_data) {
+	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm", port, pending, rssi, snr);
+	if (hex_data) {
+		LOG_HEXDUMP_INF(hex_data, len, "Payload:");
+	}
+}
 
-    if (rc != 0) {
-      printk("%s: sensor_read() failed: %d\n", dev->name, rc);
-      return rc;
-    }
+static void lorwan_datarate_changed(lorawan_datarate dr) {
+	uint8_t unused, max_size;
 
-    const struct sensor_decoder_api *decoder;
+	lorawan_get_payload_sizes(&unused, &max_size);
+	LOG_INF("New Datarate: DR_%d, Max Payload %d", dr, max_size);
+}
 
-    rc = sensor_get_decoder(dev, &decoder);
+int main(void) {
+	const struct device *lora_dev;
+	struct lorawan_join_config join_cfg;
+	uint8_t dev_eui[] = LORAWAN_DEV_EUI;
+	uint8_t join_eui[] = LORAWAN_JOIN_EUI;
+	uint8_t app_key[] = LORAWAN_APP_KEY;
+	int ret;
 
-    if (rc != 0) {
-      printk("%s: sensor_get_decode() failed: %d\n", dev->name, rc);
-      return rc;
-    }
+	struct lorawan_downlink_cb downlink_cb = {
+		.port = LW_RECV_PORT_ANY,
+		.cb = dl_callback
+	};
 
-    uint32_t temp_fit = 0;
-    struct sensor_q31_data temp_data = {0};
+	lora_dev = DEVICE_DT_GET(DT_ALIAS(lora0));
+	if (!device_is_ready(lora_dev)) {
+		LOG_ERR("%s: device not ready.", lora_dev->name);
+		return 0;
+	}
 
-    decoder->decode(buf, (struct sensor_chan_spec){SENSOR_CHAN_AMBIENT_TEMP, 0},
-                    &temp_fit, 1, &temp_data);
-    temp_buffer.push(temp_data.readings[0].temperature);
+#if defined(CONFIG_LORAMAC_REGION_EU433)
+	/* If more than one region Kconfig is selected, app should set region
+	 * before calling lorawan_start()
+	 */
+	ret = lorawan_set_region(LORAWAN_REGION_EU433);
+	if (ret < 0) {
+		LOG_ERR("lorawan_set_region failed: %d", ret);
+		return 0;
+	}
+#endif
 
-    uint32_t press_fit = 0;
-    struct sensor_q31_data press_data = {0};
+	ret = lorawan_start();
+	if (ret < 0) {
+		LOG_ERR("lorawan_start failed: %d", ret);
+		return 0;
+	}
 
-    decoder->decode(buf, (struct sensor_chan_spec){SENSOR_CHAN_PRESS, 0},
-                    &press_fit, 1, &press_data);
+	lorawan_register_downlink_callback(&downlink_cb);
+	lorawan_register_dr_changed_callback(lorwan_datarate_changed);
 
-    uint32_t hum_fit = 0;
-    struct sensor_q31_data hum_data = {0};
+	join_cfg.mode = LORAWAN_ACT_OTAA;
+	join_cfg.dev_eui = dev_eui;
+	join_cfg.otaa.join_eui = join_eui;
+	join_cfg.otaa.app_key = app_key;
+	join_cfg.otaa.nwk_key = app_key;
+	join_cfg.otaa.dev_nonce = 0u;
 
-    decoder->decode(buf, (struct sensor_chan_spec){SENSOR_CHAN_HUMIDITY, 0},
-                    &hum_fit, 1, &hum_data);
+	LOG_INF("Joining network over OTAA");
+	ret = lorawan_join(&join_cfg);
+	if (ret < 0) {
+		LOG_ERR("lorawan_join_network failed: %d", ret);
+		sys_reboot(SYS_REBOOT_WARM);
+		return 0;
+	}
 
-    printk("temp: %s%d.%d; press: %s%d.%d; humidity: %s%d.%d, elements in "
-           "buffer: %d\n",
-           PRIq_arg(temp_data.readings[0].temperature, 6, temp_data.shift),
-           PRIq_arg(press_data.readings[0].pressure, 6, press_data.shift),
-           PRIq_arg(hum_data.readings[0].humidity, 6, hum_data.shift),
-           temp_buffer.size());
+	LOG_INF("Sending data...");
+	while (1) {
+		ret = lorawan_send(2, data, sizeof(data),
+		                   LORAWAN_MSG_CONFIRMED);
 
-    k_sleep(K_MSEC(500));
-  }
-  return 0;
+		/*
+		 * Note: The stack may return -EAGAIN if the provided data
+		 * length exceeds the maximum possible one for the region and
+		 * datarate. But since we are just sending the same data here,
+		 * we'll just continue.
+		 */
+		if (ret == -EAGAIN) {
+			LOG_ERR("lorawan_send failed: %d. Continuing...", ret);
+			k_sleep(DELAY);
+			continue;
+		}
+
+		if (ret < 0) {
+			LOG_ERR("lorawan_send failed: %d", ret);
+			return 0;
+		}
+
+		LOG_INF("Data sent!");
+		k_sleep(DELAY);
+	}
 }
