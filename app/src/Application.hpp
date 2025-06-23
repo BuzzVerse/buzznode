c50c186e599554c2ee53dd40677cd431badb695c
Olaf Bykowski <olafbykowski1@gmail.com>
2025-06-23 19:09:32 +0200

    feat(application): Refactor Application class to use dependency injection for peripherals and enhance sleep management

--- app/src/Application.hpp
+++ app/src/Application.hpp
@@ -1,39 +1,49 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
-#include <etl/vector.h>
-
 #include "buzzverse/bme280.pb.h"
-#include "buzzverse/bq27441.pb.h"
 #include "buzzverse/packet.pb.h"
 
-// Forward declare to reduce include dependencies in this header
 class BME280;
 class BQ27441;
 class LoRaWANHandler;
 class SleepManager;
-class Peripheral;
-
-#ifdef CONFIG_ENABLE_DEVICE_SLEEP
-  #ifndef CONFIG_SLEEP_TIME_MS
-    #define APP_DEFAULT_SLEEP_DURATION_MS (30 * 1000)  // Default if Kconfig not set
-  #else
-    #define APP_DEFAULT_SLEEP_DURATION_MS CONFIG_SLEEP_TIME_MS
-  #endif
-#endif
 
 class Application {
  public:
-  Application();
+  /**
+   * @brief Construct a new Application object.
+   *
+   * @param bme280 Reference to the BME280 sensor.
+   * @param bq27441 Reference to the BQ27441 fuel gauge.
+   * @param lorawan Reference to the LoRaWAN handler.
+   * @param sleep_manager Pointer to the SleepManager. Can be nullptr if sleep is disabled.
+   */
+  Application(BME280& bme280, BQ27441& bq27441, LoRaWANHandler& lorawan,
+              SleepManager* sleep_manager);
   ~Application() = default;
 
   // Main phases of the application
   bool init();       // Initialize all components
   void run_cycle();  // Perform one operational cycle (read sensors, send data)
 
-#ifdef CONFIG_ENABLE_DEVICE_SLEEP
-  void enter_low_power_mode();  // Handle entering sleep
-#endif
+  /**
+   * @brief Puts the device into a low-power state.
+   *
+   * If a SleepManager was not provided during construction, this function will fall back
+   * to a simple k_sleep to prevent high power consumption in an infinite loop.
+   *
+   * @param sleep_duration_ms The duration to sleep for.
+   */
+  void enter_low_power_mode(int sleep_duration_ms);
+
+  /**
+   * @brief Populates a status packet with the initialization state of peripherals.
+   *
+   * This should be called after a failed init() to create a diagnostic report.
+   * @param packet The packet to be filled with status information.
+   */
+  void generate_init_failure_report(buzzverse_v1_Packet& packet);
 
  private:
   // Internal helper methods
@@ -41,25 +51,10 @@ class Application {
   void read_sensor_data(buzzverse_v1_BME280Data& bme_data);
   void send_lora_packet(const buzzverse_v1_BME280Data& bme_data);
 
-  // Peripheral members
   BME280& m_bme280;
   BQ27441& m_bq27441;
   LoRaWANHandler& m_lorawan;
-
-#ifdef CONFIG_ENABLE_DEVICE_SLEEP
-  SleepManager& m_sleep_manager;
-#endif
-  static constexpr uint8_t PERIPHERAL_COUNT =
-#ifdef CONFIG_ENABLE_DEVICE_SLEEP
-    4;
-#else
-    3;
-#endif
-  etl::vector<Peripheral*, PERIPHERAL_COUNT> m_peripherals;
-
-#ifdef CONFIG_ENABLE_DEVICE_SLEEP
-  int m_app_sleep_duration_ms = APP_DEFAULT_SLEEP_DURATION_MS;
-#endif
+  SleepManager* m_sleep_manager;
 };
 
 #endif  // APPLICATION_HPP
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,40 +1,69 @@
 // app/src/main.cpp
+#include <zephyr/device.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/sys/reboot.h>
 
 #include "Application.hpp"
+#include "peripherals/lorawan_handler/lorawan_handler.hpp"
+#include "sensors/bme280/bme280.hpp"
+#include "sensors/bq27441/bq27441.hpp"
 #include "utils/banner.hpp"
+#include "utils/sleep-manager.hpp"
 
 LOG_MODULE_REGISTER(main_entry, LOG_LEVEL_DBG);
 
+#ifdef CONFIG_SLEEP_TIME_MS
+  #define APP_SLEEP_DURATION_MS CONFIG_SLEEP_TIME_MS
+#else
+  #define APP_SLEEP_DURATION_MS 10000
+#endif
+
 int main(void) {
   printk("%s\n", APP_ASCII_BANNER);
-
   LOG_INF("===== Buzzverse Node System Booting (Zephyr Log) =====");
 
-  Application app;
+  BME280 bme280(DEVICE_DT_GET_ANY(bosch_bme280));
+  BQ27441 bq27441(DEVICE_DT_GET_ANY(ti_bq274xx));
+  LoRaWANHandler lorawan(bq27441);
+
+  SleepManager* p_sleep_manager = nullptr;
+
+#ifdef CONFIG_ENABLE_DEVICE_SLEEP
+  SleepManager sleep_manager_instance;
+  p_sleep_manager = &sleep_manager_instance;
+#endif
+
+  Application app(bme280, bq27441, lorawan, p_sleep_manager);
 
   if (!app.init()) {
-    LOG_ERR("Application initialization failed. Halting.");
-    while (true) {
-      k_sleep(K_SECONDS(60));
+    LOG_ERR("Critical application initialization failed!");
+
+    buzzverse_v1_Packet packet = buzzverse_v1_Packet_init_default;
+    app.generate_init_failure_report(packet);
+
+    if (lorawan.is_ready()) {
+      LOG_ERR("Attempting to send failure status packet via LoRaWAN...");
+      lorawan.send_packet(packet);
     }
+
+    LOG_ERR("Rebooting device due to critical initialization failure.");
+    sys_reboot(SYS_REBOOT_COLD);
   }
 
   app.run_cycle();
 
 #ifdef CONFIG_ENABLE_DEVICE_SLEEP
-  app.enter_low_power_mode();
+  app.enter_low_power_mode(APP_SLEEP_DURATION_MS);
   LOG_WRN("Execution continued after enter_low_power_mode - this is unexpected for deep sleep.");
 #else
-  LOG_INF("Device sleep not enabled. Running in normal mode.");
-  // Main loop for normal operation
+  LOG_INF("Device sleep not enabled. Entering polling loop.");
   while (true) {
-    k_sleep(K_SECONDS(5));
+    k_sleep(K_MSEC(APP_SLEEP_DURATION_MS));
     app.run_cycle();
   }
 #endif
 
-  return 0;
+  return 0;  // Should not be reached
 }
