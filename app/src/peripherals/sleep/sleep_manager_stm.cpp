52fd5f535c702ac6d84bba509487259f689846c4
wybran <krystian.wybranowski@gmail.com>
2026-03-23 17:09:02 +0100

    refactor: clean up formatting and improve logging in sleep manager initialization

--- app/src/peripherals/sleep/sleep_manager_stm.cpp
+++ app/src/peripherals/sleep/sleep_manager_stm.cpp
@@ -3,31 +3,29 @@
 LOG_MODULE_REGISTER(sleep_mgr_stm, LOG_LEVEL_INF);
 
 namespace {
-  volatile uint32_t wkup_count = 0;
-  volatile uint32_t last_wkup_time = 0;
+volatile uint32_t wkup_count = 0;
+volatile uint32_t last_wkup_time = 0;
 
-  void wkup_isr(const struct device* dev, struct gpio_callback* cb, uint32_t pins) {
-    uint32_t current_time = k_uptime_get_32();
-    
-    if (current_time - last_wkup_time > 250) {
-      last_wkup_time = current_time;
-      wkup_count++;
-    }
+void wkup_isr(const struct device* dev, struct gpio_callback* cb, uint32_t pins) {
+  uint32_t current_time = k_uptime_get_32();
+
+  if (current_time - last_wkup_time > 250) {
+    last_wkup_time = current_time;
+    wkup_count++;
   }
 }
+}  // namespace
 
 extern struct k_sem wakeup_sem;
 
-#define WAKEUP_PIN_CFG(node_id, prop, idx) \
-  GPIO_DT_SPEC_GET_BY_IDX(node_id, prop, idx),
+#define WAKEUP_PIN_CFG(node_id, prop, idx) GPIO_DT_SPEC_GET_BY_IDX(node_id, prop, idx),
 
 SleepManagerStm::SleepManagerStm()
-    : initialized(false),
-      sleep_timeout_ms(DEFAULT_SLEEP_DURATION_MS) {
+    : initialized(false), sleep_timeout_ms(DEFAULT_SLEEP_DURATION_MS) {
 #ifdef CONFIG_SOC_STM32WL55XX
+  #if DT_NODE_HAS_PROP(DT_PATH(zephyr_user), wakeup_gpios)
   static const struct gpio_dt_spec specs[] = {
-    DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), wakeup_gpios, WAKEUP_PIN_CFG)
-  };
+    DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), wakeup_gpios, WAKEUP_PIN_CFG)};
 
   for (size_t i = 0; i < ARRAY_SIZE(specs); i++) {
     if (!wakeup_pins.full()) {
@@ -35,15 +33,22 @@ SleepManagerStm::SleepManagerStm()
       pin.spec = specs[i];
       wakeup_pins.push_back(pin);
     } else {
-      LOG_WRN("Max wakeup pins reached, skipping some.");
+      LOG_WRN("Max wakeup pins reached.");
       break;
     }
   }
+  #else
+  LOG_WRN("No wakeup-gpios defined in zephyr,user node.");
+  #endif
 #endif
 }
 
 Peripheral::Status SleepManagerStm::init() {
 #ifdef CONFIG_SOC_STM32WL55XX
+  if (wakeup_pins.empty()) {
+    LOG_INF("No wakeup pins to configure.");
+  }
+
   for (auto& pin : wakeup_pins) {
     if (!device_is_ready(pin.spec.port)) {
       LOG_ERR("GPIO port %s not ready", pin.spec.port->name);
@@ -58,7 +63,7 @@ Peripheral::Status SleepManagerStm::init() {
     gpio_pin_interrupt_configure_dt(&pin.spec, GPIO_INT_EDGE_TO_ACTIVE);
     gpio_init_callback(&pin.cb_data, wkup_isr, BIT(pin.spec.pin));
     gpio_add_callback(pin.spec.port, &pin.cb_data);
-    
+
     LOG_INF("Wakeup source initialized: Pin %d on %s", pin.spec.pin, pin.spec.port->name);
   }
 
